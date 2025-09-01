1282b766737a79d3dac2fa34c26f7ae40f20549a
leandro <leandro@gmail.com>
2025-09-01 17:49:24 +0200

    Fixed routing

--- app/components/ContactUs/ContactUs.tsx
+++ app/components/ContactUs/ContactUs.tsx
@@ -16,20 +16,21 @@ const ContactUs: React.FC = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const res = await fetch('/api/contact', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(form),
-      });
-      const data = await res.json();
-      if (res.ok) {
-        alert('Message sent!');
-        setForm({ firstName: '', lastName: '', email: '', message: '' });
-      } else {
-        alert('Failed to send message: ' + (data.message || 'Unknown error'));
-      }
+      // const res = await fetch('/api/contact', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify(form),
+      // });
+      // const data = await res.json();
+      // if (res.ok) {
+      //   alert('Message sent!');
+      //   setForm({ firstName: '', lastName: '', email: '', message: '' });
+      // } else {
+      //   alert('Failed to send message: ' + (data.message || 'Unknown error'));
+      // }
+      console.log("Handling mail")
     } catch (error) {
       alert('Failed to send message: ' + error);
     }
